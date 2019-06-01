fd9f3140b088852289332fb48dca0bb01756cc10
David van der Tuijn <david@floro.nl>
2019-06-01 17:06:10 +0200

    Added Not Implemented Exception

--- src/number_format_currency.php
+++ src/number_format_currency.php
@@ -3,24 +3,27 @@
 /**
  * Number Format Currency.
  *
- * @param mixed $sValue
+ * @param float $fNumber
  * @param int $iDecimals
  * @param mixed $sCurrency
  *
  * @return string
  */
-function number_format_currency(?string $sValue, int $iDecimals = 2, ?string $sCurrency): string
+function number_format_currency(?float $fNumber, int $iDecimals = 2, ?string $sCurrency): string
 {
-    $fValue = number_format_to_decimal($sValue);
-
-    switch ($sCurrency) {
-        case 'EUR':
-            return $fValue ? number_format($fValue, $iDecimals, ',', '.') : number_format(0, $iDecimals, ',', '.');
-        case 'USD':
-            return $fValue ? number_format($fValue, $iDecimals, '.', ',') : number_format(0, $iDecimals, '.', ',');
-        case 'GBP':
-            return $fValue ? number_format($fValue, $iDecimals, ',', '.') : number_format(0, $iDecimals, ',', '.');
-        default:
-            return $fValue ? number_format($fValue, $iDecimals, ',', '.') : number_format(0, $iDecimals, ',', '.');
+    if (!is_float($fNumber)) {
+        $fNumber = 0;
     }
+
+    $aCurrencies = [
+        'EUR' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'USD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'GBP' => ['dec_point' => ',', 'thousands_sep' => '.']
+    ];
+
+    if (!array_key_exists($sCurrency, $aCurrencies)) {
+        throw new \Exception('Not Implemented');
+    }
+
+    return number_format($fNumber, $iDecimals, $aCurrencies[$sCurrency]['dec_point'], $aCurrencies[$sCurrency]['thousands_sep']);
 }
