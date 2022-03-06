d15ef1d6daa2dba8f6c6392569c88663ff438e21
David van der Tuijn <david@digitalimpact.nl>
2022-03-06 22:50:24 +0100

    Currencies, Notation

--- README.md
+++ README.md
@@ -4,7 +4,44 @@
 <a href="https://packagist.org/packages/davidvandertuijn/number_format_currency"><img src="https://poser.pugx.org/davidvandertuijn/number_format_currency/v/stable.svg" alt="Latest Stable Version"></a>
 <a href="https://packagist.org/packages/davidvandertuijn/number_format_currency"><img src="https://poser.pugx.org/davidvandertuijn/number_format_currency/license.svg" alt="License"></a>
 
-Currently ONLY supporting the following currencies: EUR, USD and GBP.
+Support for the currencies below:
+
+| Country            | Currency           | Abbreviation/Code |
+|--------------------|--------------------|-------------------|
+| Argentina          | Argentine Peso     | ARS               |
+| Australia          | Australian Dollar  | AUD               |
+| Brazil             | Brazilian Real     | BRL               |
+| Chile              | Chilean Peso       | CLP               |
+| China              | Yuan Renminbi      | CNY               |
+| Columbia           | Colombian Peso     | COP               |
+| Czech Republic     | Czech Koruna       | CZK               |
+| Denmark            | Danish Krone       | DKK               |
+| European Union     | Euro               | EUR               |
+| Hong Kong          | Hong Kong Dollar   | HKD               |
+| Hungary            | Hungarian Forint   | HUF               |
+| India              | Indian Rupee       | INR               |
+| Israel             | New Israeli Shekel | ILS               |
+| Japan              | Yen                | JPY               |
+| Korea, Republic of | Won                | KRW               |
+| Malaysia           | Malaysian Ringgit  | MYR               |
+| Mexico             | Mexican Peso       | MXN               |
+| Morocco            | Moroccan Dirham    | MAD               |
+| New Zealand        | New Zealand Dollar | NZD               |
+| Norway             | Norwegian Krone    | NOK               |
+| Philippines        | Philippine Peso    | PHP               |
+| Poland             | Zloty              | PLN               |
+| Russian Federation | Russian Ruble      | RUB               |
+| Saudi Arabia       | Saudi Riyal        | SAR               |
+| Singapore          | Singapore Dollar   | SGD               |
+| South Africa       | Rand               | ZAR               |
+| Sweden             | Swedish Krona      | SEK               |
+| Switzerland        | Swiss Franc        | CHF               |
+| Taiwan             | New Taiwan Dollar  | TWD               |
+| Thailand           | Baht               | THB               |
+| Turkey             | Turkish Lira       | TRY               |
+| UK/Great Britain   | Pound Sterling     | GBP               |
+| United States      | US Dollar          | USD               |
+| Vietnam            | Dong               | VND               |
 
 ## Install
 
@@ -15,10 +52,18 @@ composer require davidvandertuijn/number_format_currency
 ## Usage
 
 ```php
-number_format_currency('1000.99', 2, 'EUR'); // 1.000,99
-number_format_currency('1000.99', 2, 'USD'); // 1,000.99
+number_format_currency('1234.56', 2, 'EUR'); // 1.234,56
+number_format_currency('1234.56', 2, 'USD'); // 1,234.56
 ```
 
-### Comments
+### English or French Notation
 
-This is a simple formatter, a more advanced formatter is <a href="https://php.net/money_format">money_format</a>.
+Canada (English)
+```php
+echo number_format_currency('1234.56', 2, 'CAD', 'English'); // 1,234.56
+```
+
+Canada (French)
+```php
+echo number_format_currency('1234.56', 2, 'CAD', 'French'); // 1 234,56
+```
--- src/number_format_currency.php
+++ src/number_format_currency.php
@@ -3,28 +3,69 @@
 /**
  * Number Format Currency.
  *
- * @param float|null $fNumber
- * @param int $iDecimals
- * @param string|null $sCurrency
+ * @param float|null $value
+ * @param int $decimals
+ * @param string|null $currency
+ * @param string|null $notation
  *
  * @return string|null
  * @throws \Exception
  */
-function number_format_currency(?float $fNumber, int $iDecimals = 2, ?string $sCurrency)
+function number_format_currency(?float $value, int $decimals = 2, ?string $currency = null, ?string $notation = null)
 {
-    if (is_null($sValue)) {
+    if (is_null($value)) {
         return null;
     }
 
-    $aCurrencies = [
-        'EUR' => ['dec_point' => ',', 'thousands_sep' => '.'],
-        'USD' => ['dec_point' => '.', 'thousands_sep' => ','],
-        'GBP' => ['dec_point' => ',', 'thousands_sep' => '.']
-    ];
-
-    if (!array_key_exists($sCurrency, $aCurrencies)) {
-        throw new \Exception('Not Implemented');
+    if (!is_null($notation)) {
+        switch ($notation) {
+            case 'English':
+                return number_format($value, $decimals, '.', ',');
+            case 'French':
+                return number_format($value, $decimals, ',', ' ');
+        }
     }
 
-    return number_format($fNumber, $iDecimals, $aCurrencies[$sCurrency]['dec_point'], $aCurrencies[$sCurrency]['thousands_sep']);
+    $currencies = [
+        'ARS' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'AUD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'BRL' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'CHF' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'CLP' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'CNY' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'COP' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'CZK' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'DKK' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'EUR' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'GBP' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'HKD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'HUF' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'ILS' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'INR' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'JPY' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'KRW' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'MAD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'MXN' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'MYR' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'NOK' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'NZD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'PHP' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'PLN' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'RUB' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'SAR' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'SEK' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'SGD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'THB' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'TRY' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'TWD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'USD' => ['dec_point' => '.', 'thousands_sep' => ','],
+        'VND' => ['dec_point' => ',', 'thousands_sep' => '.'],
+        'ZAR' => ['dec_point' => '.', 'thousands_sep' => ','],
+    ];
+
+    if (!array_key_exists($currency, $currencies)) {
+        throw new \Exception('Currency Not Implemented');
+    }
+
+    return number_format($value, $decimals, $currencies[$currency]['dec_point'], $currencies[$currency]['thousands_sep']);
 }
